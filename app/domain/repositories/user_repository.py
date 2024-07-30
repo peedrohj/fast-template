2dcde5897d8770581bb74af29450b1cc8bb0bdc3
Pedro Farias <phjfg@cesar.org.br>
2024-07-29 21:38:02 -0300

    feat: add other methods to user repository inteface

--- app/domain/repositories/user_repository.py
+++ app/domain/repositories/user_repository.py
@@ -1,7 +1,18 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+from typing import List
 
 from app.domain.entities.user import User
 
 
 class UserRepository(ABC):
+    @abstractmethod
     def save(self, user: User, session: any = None) -> User: ...
+
+    @abstractmethod
+    def list(self, session: any = None) -> List[User]: ...
+
+    @abstractmethod
+    def find(self, id: int, session: any = None) -> User: ...
+
+    @abstractmethod
+    def delete(self, id: int, session: any = None) -> None: ...
